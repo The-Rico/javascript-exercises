a7fc0ca8323e1920ec60c48c62e992ed1d2ff27a
The-Rico <rico21r@gmail.com>
2022-01-28 20:06:34 -0800

    Finished JAVASCRIPT TUTORIALS

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
 
-};
+const repeatString = function hey(input, n) {
+  if (n == "") {
+    return "";
+    }
+    else if (n < 1) {
+      return "ERROR";
+    }
 
+  let x = input
+    for (let i=1; i < n; i++) {
+      x += input
+    }
+    return x;
+  }
 // Do not edit below this line
 module.exports = repeatString;
+//for (let i = 0; i < hey.length, i++); {
+//}
